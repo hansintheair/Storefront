240490734a938a4449ff62b34479a8ccfab7befa
hansintheair <40738670+hansintheair@users.noreply.github.com>
2024-05-19 15:36:44 -0700

    Basic login func. implemented (#1)
    * Basic login func. implemented
    
    * register new account

---
+++ storefront/StoreDB.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+/**
+ * Description of db_access
+ *
+ * @author hanne
+ */
+class StoreDB {
+
+    private $servername = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $db_name = "storefront";
+    private $db;
+    
+    function connect() {
+        $this->db = new mysqli(
+            $this->servername,
+            $this->username,
+            $this->password,
+            $this->db_name
+        );
+        
+        echo "Connecting to ".$this->db_name."... ";
+        
+        // Check connection
+        if ($this->db->connect_error) {
+            die("Connection failed<br>".$this->db->connect_error);
+        }
+        
+        echo "Succeeded<br>";
+    }
+    
+    function disconnect() {
+        
+        echo "Disconnecting from ".$this->db_name."... ";
+
+        if (!$this->db) {
+            echo "Nothing to disconnect.<br>";
+            return;
+        }
+        
+        if ($this->db->close()) {
+            echo "Succeeded<br>";
+        }
+        else {
+            echo "Failed<br>";
+        }
+    }
+    
+    function checkUserExists($email) {
+        $query = "SELECT * FROM `storefront`.`entity_users` AS `entity_users` WHERE `email` = '".$email."'";
+
+        return (bool)$this->db->query($query)->fetch_assoc();
+    }
+    
+    function getUser($email) {
+        $query = "SELECT `id_user` AS `USER_UID`, `email` AS `EMAIL`, `password` AS `PASSWORD` FROM `storefront`.`entity_users` AS `entity_users` WHERE `email` = '".$email."'";
+    
+        return $this->db->query($query)->fetch_assoc();
+    }
+    
+    function addUser($email, $password) {
+        $query = "INSERT INTO `storefront`.`entity_users` (`email`, `password`) VALUES ('".$email."', '".$password."')";
+        
+        $this->db->query($query);
+    }
+
+}
