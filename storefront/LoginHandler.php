793027e3596675d83421d7c6ca3c0ea725186ead
hansintheair <40738670+hansintheair@users.noreply.github.com>
2024-05-19 15:49:38 -0700

    cleanup (#2)

--- storefront/LoginHandler.php
+++ storefront/LoginHandler.php
@@ -12,19 +12,19 @@ $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
 //echo "email: ".$email."<br>";  //DEBUG
 //echo "PASSWORD: ".$password."<br>";  //DEBUG
  
-// Verify user credentials and redirect
-$userdata;
- if ($email && $password) {
-     $store_db->connect();
-     $userdata = $store_db->getUser($email);
-     $store_db->disconnect();     
-    if ($userdata["EMAIL"] == $email && password_verify($password, $userdata["PASSWORD"])) {
-        $_SESSION["login_error"] = "WOHOO! PASSWORD VERIFIED!";  //DEBUG
-        header("Location: Home.php");  //DEBUG
-//        header("Location: User.php");  //NOT IMPLEMENTED
-        exit;
-    }
- }
-$_SESSION["login_error"] = "Invalid email or password";
-header("Location: Home.php");
+// Verify user credentials
+$store_db->connect();
+$userdata = $store_db->getUser($email);
+$store_db->disconnect();     
+if (!($userdata["EMAIL"] == $email && password_verify($password, $userdata["PASSWORD"]))) {
+    $_SESSION["login_error"] = "Invalid email or password";
+    header("Location: Home.php");
+   exit;
+}
+
+// Login redirect
+$_SESSION["login_error"] = "WOHOO! PASSWORD VERIFIED!";  //DEBUG
+header("Location: Home.php");  //DEBUG
+//header("Location: User.php");  //NOT IMPLEMENTED
+
 
