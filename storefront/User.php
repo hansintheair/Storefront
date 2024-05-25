c03b6e5e676509de1cf3dc3deba1cb18ab023064
hansintheair <hannesz1@gmail.com>
2024-05-25 13:15:32 -0700

    Make the user navbar a reusable component

--- storefront/User.php
+++ storefront/User.php
@@ -20,17 +20,7 @@
     <body>
         
         <div class="navbar">
-            <div class="shopping">
-                <div class="active"><a href="#about">Shop</a></div>
-                <div class="searchbar"><input type="text" placeholder="Search..."></div>
-                <div><a href="#cart">Cart</a></div>
-                <div><a href="#orders">Orders</a></div>
-            </div>
-            <div class="account">
-                <div><a href="#profile">Profile</a></div>
-                <div class="logout"><a href="LogoutHandler.php">Logout</a></div>
-            </div>
-            
+            <?php include 'UserNavbar.php';?>
         </div>
         
         <div class="items_pane">
---
+++ storefront/UserNavbar.php
@@ -0,0 +1,10 @@
+<div class="shopping">
+    <div class="active"><a href="#about">Shop</a></div>
+    <div class="searchbar"><input type="text" placeholder="Search..."></div>
+    <div><a href="#cart">Cart</a></div>
+    <div><a href="#orders">Orders</a></div>
+</div>
+<div class="account">
+    <div><a href="#profile">Profile</a></div>
+    <div class="logout"><a href="LogoutHandler.php">Logout</a></div>
+</div>
