95bec2966ded4d10acec79ea5b0fd32d7c542b7b
hansintheair <hannesz1@gmail.com>
2024-05-25 15:11:27 -0700

    profile layout WIP

--- storefront/UserProfile.php
+++ storefront/UserProfile.php
@@ -1,5 +1,5 @@
 <?php
-    include "SecureSession.php";
+    include "SecureSession.php"
 ?>
 
 <!DOCTYPE html>
@@ -9,6 +9,7 @@
         <title>User Profile</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="stylesheet" href="navbar.css">
+        <link rel="stylesheet" href="user_profile.css">
     </head>
     
     <body>
@@ -20,7 +21,24 @@
             document.getElementsByClassName("orders")[0].style.display = "none";
         </script>
         
+        <div id="profile_info">
+            <h3>Account Info</h3>
+            <p><b>Email</b><br><span id="email">test</span></p>
+            
+        </div>
         
+        <div id="profile_options">
+            <h3>Options</h3>
+            <ul>
+                <li><a>Change email</a></li>
+                <li><a>Change password</a></li>
+                <li><p id="dangerzone"><b>Danger Zone</b></p></li>
+                <li><a>Delete Account</a></li>
+            </ul>
+            
+                
+        </div>
+    
     </body>
     
 </html>
