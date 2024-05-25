e4204605c3c25d216749da7f72d78a8d0aa7e847
hansintheair <hannesz1@gmail.com>
2024-05-25 07:27:19 -0700

    add. create account input validation

--- storefront/CreateAccount.php
+++ storefront/CreateAccount.php
@@ -12,11 +12,10 @@
     <body>
         
         <h1>Create Your Big4Shopping Account</h1>
-        
         <div>
             <form action="CreateAccountHandler.php" method="post">
-                E-mail: <input type="email" name="email" required><br>
-                Password: <input type="password" name="password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$%*?&])[A-Za-z\\d@$%*?&]{8,}$" title="Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character @$%*? or &" required><br>
+                E-mail: <input type="email" name="email" pattern="^[_A-Za-z0-9-]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\.[A-Za-z0-9]+)*(\.[A-Za-z]{2,})$" title="email must have an @ symbal and a dot ." required><br>
+                Password: <input type="password" name="password" pattern="((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,20})" title="Password must be at between 8 and 20 characters long, contain at least one of each: uppercase letter, lowercase letter, number, and special character !@#$%^&*" required><br>
                 <input type="submit" value="Register">
                 <span id="error"><?php echo isset($_SESSION['register_error']) ? $_SESSION['register_error'] : "";?></span>
             </form>
@@ -24,6 +23,7 @@
         
         <div id="registered">
             <?php echo isset($_SESSION['register_success']) ? "Successfully registered" : "";?>
+            <span>Already have an account?</span> <a href="Home.php">Sign in</a>
         </div>
         
     </body>
--- storefront/CreateAccountHandler.php
+++ storefront/CreateAccountHandler.php
@@ -13,6 +13,22 @@ $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
 //echo "PASSWORD: ".$password."<br>";  //DEBUG
 //echo "EMAIL: ".$email."<br>";  //DEBUG
 
+// Validate password complexity
+$pword_pattern = "((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,20})";
+if (!preg_match($pword_pattern, $password)) {
+    $_SESSION["register_error"] = "password does not meet required complexity";
+    header("Location: CreateAccount.php");
+    exit;
+}
+
+// Validate email format
+$email_pattern = "^[_A-Za-z0-9-]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\.[A-Za-z0-9]+)*(\.[A-Za-z]{2,})$";
+if (!preg_match($email_pattern, $email)) {
+    $_SESSION["register_error"] = "invalid e-mail format";
+    header("Location: CreateAccount.php");
+    exit;
+}
+
 // Check if user is already in the database
 $store_db->connect();
 $exists = $store_db->checkUserExists($email);
@@ -25,7 +41,7 @@ if ($exists) {
 
 // Validate password
 if (!$password) {  //TODO: Could use some extra validation
-    $_SESSION["register_error"] = "Bad password";
+    $_SESSION["register_error"] = "Account and password do not match";
     header("Location: CreateAccount.php");
     exit;
 }
@@ -37,6 +53,7 @@ $store_db->addUser($email, $password_hashed);
 $store_db->disconnect();
 
 $_SESSION["register_success"] = true;
+$_SERVER["register_error"] = "";
 header("Location: CreateAccount.php");
 
  
