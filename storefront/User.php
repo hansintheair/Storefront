186a728d9450084aaa70f5dd3ee8b6702eca92a8
hansintheair <hannesz1@gmail.com>
2024-05-25 07:58:00 -0700

    improved navbar formatting

--- storefront/User.php
+++ storefront/User.php
@@ -9,15 +9,20 @@
     <body>
         
         <div class="navbar">
-            <div class="active"><a href="#about">Home</a></div>
-            <div class="searchbar"><input type="text" placeholder="Search..."></div>
-            <div><a href="#cart">Cart</a></div>
-            <div><a href="#orders">Orders</a></div>
-            <div><a href="#profile">Profile</a></div>
-            <div class="logout"><a href="#logout">Logout</a></div>
+            <div class="shopping">
+                <div class="active"><a href="#about">Home</a></div>
+                <div class="searchbar"><input type="text" placeholder="Search..."></div>
+                <div><a href="#cart">Cart</a></div>
+                <div><a href="#orders">Orders</a></div>
+            </div>
+            <div class="account">
+                <div><a href="#profile">Profile</a></div>
+                <div class="logout"><a href="#logout">Logout</a></div>
+            </div>
+            
         </div>
         
-        <div class="catalog">
+        <div class="items">
             
         </div>
         
