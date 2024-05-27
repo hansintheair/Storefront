27365583e2af656cdc6a2982a99e1cb79eb7ded0
hansintheair <hannesz1@gmail.com>
2024-05-27 15:39:33 -0700

    Style the login/create user pages

--- storefront/CreateAccount.php
+++ storefront/CreateAccount.php
@@ -8,22 +8,25 @@
         <title>Create Your Big4Shopping Account</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="stylesheet" href="styles/messages.css">
+        <link rel="stylesheet" href="styles/home.css">
     </head>
     <body>
         
-        <h1>Create Your Big4Shopping Account</h1>
+        <h1>Big4Shopping</h1>
+        <h3>Create Your Account</h3>
         <div>
-            <form action="CreateAccountHandler.php" method="post">
-                E-mail: <input type="email" name="email" pattern="^[A-Za-z0-9._\-]+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}$" title="invalid email address format" required><br>
-                Password: <input type="password" name="password" pattern="((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,20})" title="Password must be at between 8 and 20 characters long, contain at least one of each: uppercase letter, lowercase letter, number, and special character !@#$%^&*" required><br>
+            <form class="menu-container" action="CreateAccountHandler.php" method="post">
+                E-mail<input type="email" name="email" pattern="^[A-Za-z0-9._\-]+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}$" title="invalid email address format" required><br>
+                Password<input type="password" name="password" pattern="((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,20})" title="Password must be at between 8 and 20 characters long, contain at least one of each: uppercase letter, lowercase letter, number, and special character !@#$%^&*" required><br>
                 <input type="submit" value="Register">
                 <span class="error"><?php echo isset($_SESSION["register_error"]) ? $_SESSION["register_error"] : "";?></span>
             </form>
         </div>
         
-        <div id="registered">
-            <p><?php echo isset($_SESSION["register_success"]) ? "Successfully registered" : "";?></p>
-            <span>Already have an account?</span> <a href="Home.php">Log in</a>
+        <div>            
+            <span id="registered"><?php echo isset($_SESSION["register_success"]) ? "Successfully registered" : "";?></span>
+            <h3>Already have an account?</h3>
+            <button onclick="location.href='Home.php'">Log in</button>
         </div>
         
     </body>
--- storefront/Home.php
+++ storefront/Home.php
@@ -8,27 +8,36 @@
         <title>Big4Shopping</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="stylesheet" href="styles/messages.css">
+        <link rel="stylesheet" href="styles/home.css">
     </head>
     
     <body>
         
         <h1>Big4Shopping</h1>
+        <h3>Log in to your Account</h3>
         
         <div>
-            <form action="LoginHandler.php" method="post">
-                E-mail: <input type="email" name="email" pattern="^[A-Za-z0-9._\-]+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}$" title="invalid email address format" required><br>
-                Password: <input type="password" name="password" required><br>
+            <form class="menu-container" action="LoginHandler.php" method="post">
+                <!--<label for="email">Email</label><br>-->
+                E-mail<input type="email" name="email" pattern="^[A-Za-z0-9._\-]+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}$" title="invalid email address format" required><br>
+                <!--<label for="password">Password</label><br>-->
+                Password<input type="password" name="password" required><br>
                 <input type="submit" value="Log in">
                 <span class="error" id="error"><?php echo isset($_SESSION['login_error']) ? $_SESSION['login_error'] : "";?></span>
             </form>
         </div>
         
         <div>
+            <span id="registered"></span>
             <h3>Don't have an account?</h3>
             <button type="button" onclick="location.href='\CreateAccount.php'">Create Account</button>
         </div>
         
     </body>
+    
+    <?php
+        unset($_SESSION['login_error']);
+    ?>
     
 </html>
 
