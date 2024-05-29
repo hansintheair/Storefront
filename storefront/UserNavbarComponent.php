77f82b6bd17b984c66acf8727cbdb0593cf8b532
hansintheair <hannesz1@gmail.com>
2024-05-28 18:44:46 -0700

    Add stub for orders tab

--- storefront/UserNavbarComponent.php
+++ storefront/UserNavbarComponent.php
@@ -3,7 +3,7 @@
         <div class="active"><a href="UserCatalog.php">Shop</a></div>
         <div class="searchbar"><input type="text" placeholder="Search..."></div>
         <div class="cart"><a href="UserCart.php">Cart</a></div>
-        <div class="orders"><a href="#orders">Orders</a></div>
+        <div class="orders"><a href="UserOrders.php">Orders</a></div>
     </div>
     <div class="account">
         <div><a href="UserProfile.php">Profile</a></div>
---
+++ storefront/UserOrders.php
@@ -0,0 +1,31 @@
+<?php
+    include "SecureSessionComponent.php";
+?>
+
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <title>User Profile</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="stylesheet" href="styles/navbar.css">
+        <link rel="stylesheet" href="styles/catalog.css">
+        <script type="text/javascript" src="DisplayItemController.js"></script>
+    </head>
+    
+    <body>
+        
+        <?php include 'UserNavbarComponent.php';?>
+        
+        <div class="items_pane">
+            <div class="sidebar"></div>
+            <div class="items_list">
+                <script>
+                    
+                </script>
+            </div>
+        </div>
+        
+    </body>
+    
+</html>
