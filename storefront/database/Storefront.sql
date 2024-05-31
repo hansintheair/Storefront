dffcd58323fb7c504f1e3ea3a423818241b78318
hansintheair <hannesz1@gmail.com>
2024-05-31 10:32:01 -0700

    fix field name

--- storefront/database/Storefront.sql
+++ storefront/database/Storefront.sql
@@ -29,7 +29,7 @@ CREATE TABLE `entity_cartitems` (
   `id_item` mediumint(7) DEFAULT NULL,
   `quant` mediumint(7) DEFAULT NULL,
   PRIMARY KEY (`id_cartitem`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +38,7 @@ CREATE TABLE `entity_cartitems` (
 
 LOCK TABLES `entity_cartitems` WRITE;
 /*!40000 ALTER TABLE `entity_cartitems` DISABLE KEYS */;
-INSERT INTO `entity_cartitems` VALUES (1,3,5),(2,7,1),(3,8,1);
+INSERT INTO `entity_cartitems` VALUES (1,3,5),(25,1,2),(26,4,1),(27,3,5),(28,8,1);
 /*!40000 ALTER TABLE `entity_cartitems` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -89,7 +89,7 @@ CREATE TABLE `entity_items` (
 
 LOCK TABLES `entity_items` WRITE;
 /*!40000 ALTER TABLE `entity_items` DISABLE KEYS */;
-INSERT INTO `entity_items` VALUES (1,'Toothbrush','(1) generic toothbrush',1),(2,'Toothpaste','(1) generic toothpaste',2),(3,'Soap','(1) generic bar soap',3),(4,'Tissue Paper','(1) generic tissue paper',4),(5,'Shower Sponge','(1) generic shower sponge',5),(6,'Nail Clipper','(1) quality nail clipper',6),(7,'Shampoo','(1) generic shampoo',7),(8,'Apples','(6) tasy apples',8),(9,'Oranges','(4) juicy oranges',9);
+INSERT INTO `entity_items` VALUES (1,'Toothbrush','Generic toothbrush',1),(2,'Toothpaste','Generic toothpaste',2),(3,'Soap','Generic bar soap',3),(4,'Tissue Paper','Generic tissue paper',4),(5,'Shower Sponge','Generic shower sponge',5),(6,'Nail Clipper','Quality nail clipper',6),(7,'Shampoo','Generic shampoo',7),(8,'Apples','Bag of 6 tasy apples',8),(9,'Oranges','Bag of 4 juicy oranges',9);
 /*!40000 ALTER TABLE `entity_items` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -128,7 +128,7 @@ DROP TABLE IF EXISTS `entity_orders`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `entity_orders` (
   `id_order` mediumint(7) NOT NULL AUTO_INCREMENT,
-  `oder_date` datetime DEFAULT NULL,
+  `order_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id_order`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -156,7 +156,7 @@ CREATE TABLE `entity_users` (
   `password` varchar(255) DEFAULT NULL,
   `type` tinyint(3) DEFAULT NULL,
   PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -165,7 +165,7 @@ CREATE TABLE `entity_users` (
 
 LOCK TABLES `entity_users` WRITE;
 /*!40000 ALTER TABLE `entity_users` DISABLE KEYS */;
-INSERT INTO `entity_users` VALUES (8,'hansintheair@email.com','$2y$10$ok101JjiFWgTyAF5mvGPj.NQo1hxVtEKZzOoklhGnHnNBfcVTILWe',0);
+INSERT INTO `entity_users` VALUES (8,'hansintheair@email.com','$2y$10$RpzaeV1hBUkwCTdP7zMg0uE9ep.m5HxxIseENoURASZ7Xsle4A4du',0),(16,'test@email.com','$2y$10$vR6lua8xXNz08ib.f4N5iuscyWWofD/6VpqjgPPYhqyhCzl7gxHdC',0),(17,'test2@email.com','$2y$10$A0XuWuJTOpmHYDAs.etJTe0kxKOvxheRcDnH1XtZwrnYoIE6S09D6',0);
 /*!40000 ALTER TABLE `entity_users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -206,7 +206,7 @@ CREATE TABLE `xref_users_cartitems` (
   `id_user` int(10) DEFAULT NULL,
   `id_cartitem` int(10) DEFAULT NULL,
   PRIMARY KEY (`id_user_cartitem`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -215,7 +215,7 @@ CREATE TABLE `xref_users_cartitems` (
 
 LOCK TABLES `xref_users_cartitems` WRITE;
 /*!40000 ALTER TABLE `xref_users_cartitems` DISABLE KEYS */;
-INSERT INTO `xref_users_cartitems` VALUES (1,8,1),(2,8,2),(3,8,3);
+INSERT INTO `xref_users_cartitems` VALUES (15,8,1),(30,16,25),(31,16,26),(32,16,27),(33,8,28);
 /*!40000 ALTER TABLE `xref_users_cartitems` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -240,7 +240,7 @@ CREATE TABLE `xref_users_orders` (
 
 LOCK TABLES `xref_users_orders` WRITE;
 /*!40000 ALTER TABLE `xref_users_orders` DISABLE KEYS */;
-INSERT INTO `xref_users_orders` VALUES (1,1,1);
+INSERT INTO `xref_users_orders` VALUES (1,8,1);
 /*!40000 ALTER TABLE `xref_users_orders` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -253,4 +253,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-26  5:04:22
+-- Dump completed on 2024-05-31 10:31:07
